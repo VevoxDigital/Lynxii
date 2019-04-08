5a802e15eb5dacaa43deb846f4c70ed4fc8c4948
Matthew <matt@vevox.io>
2019-04-08 12:52:50 -0500

    more config work, pretty much empty the project

--- src/common/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as DeviceAddress } from './util/device-address'
-export { default as errorList } from './errors'
-export * from './util'
--- src/server/index.ts
+++
@@ -1,6 +0,0 @@
-import { onServerMessage } from './core/messages'
-import LynxiiServer from './core/server'
-
-const server = new LynxiiServer()
-server.events.on('message', onServerMessage)
-server.message('hello world!')
