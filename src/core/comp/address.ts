3963714dcded0316ce7c12e1f637499bd5971de5
Matthew <matt@vevox.io>
2019-06-03 12:50:04 -0500

    swap the port data and number octets

--- src/core/comp/address.ts
+++ src/core/comp/address.ts
@@ -7,6 +7,33 @@ export class ComponentAddress extends AbstractAddress {
   public static readonly GROUP_COUNT = 8
   public static readonly BIT_LENGTH = 64
 
+  /** Number of bits to shift to make the OUI the least significant digits */
+  public static readonly SHIFT_OUI = ComponentAddress.GROUP_WIDTH * 5
+
+  /** Number of bits to shify to make the NIC the least significant digits */
+  public static readonly SHIFT_NIC = ComponentAddress.GROUP_WIDTH * 2
+
+  /** Number of bits to shift to make the TYPE the least significant digits */
+  public static readonly SHIFT_TYP = ComponentAddress.GROUP_WIDTH
+
+  /** Number of bits to shift to make the NUMBER the least significant digits */
+  public static readonly SHIFT_NUM = 0
+
+  /** Mask for isolating the OUI */
+  public static readonly MASK_OUI = 0xFFFFFFn << BigInt(ComponentAddress.SHIFT_OUI)
+
+  /** Mask for isolating the NIC */
+  public static readonly MASK_NIC = 0xFFFFFFn << BigInt(ComponentAddress.SHIFT_NIC)
+
+  /** Mask for isloating the TYPE */
+  public static readonly MASK_TYP = 0xFFn << BigInt(ComponentAddress.SHIFT_TYP)
+
+  /** Mask for isolating the NUMBER */
+  public static readonly MASK_NUM = 0xFFn << BigInt(ComponentAddress.SHIFT_NUM)
+
+  public static readonly MASK_ID = ComponentAddress.MASK_TYP | ComponentAddress.MASK_NUM
+  public static readonly MASK_MAC = ComponentAddress.MASK_OUI | ComponentAddress.MASK_NIC
+
   /**
    * Creates a new component address from the given six-octect and two-octect addresses
    * @param comp The component physical address
@@ -14,19 +41,30 @@ export class ComponentAddress extends AbstractAddress {
    */
   public static fromAddresses (comp: MACAddress | bigint, port: number = 0) {
     comp = comp instanceof MACAddress ? comp.value : comp
-    return new ComponentAddress((comp << 16n) & BigInt(port & 0xFFFF))
+    return new ComponentAddress((comp << BigInt(ComponentAddress.SHIFT_NIC))
+      & (BigInt(port) & ComponentAddress.MASK_ID))
   }
 
+  public readonly OUI: bigint
+  public readonly NIC: bigint
+  public readonly TYP: bigint
+  public readonly NUM: bigint
+
   /** The six-octect address of the component itself */
-  public readonly component: MACAddress
+  public readonly componentMAC: MACAddress
 
   /** The two-octect address of the port */
-  public readonly port: number
+  public readonly id: bigint
 
   public constructor (address: bigint) {
     super(ComponentAddress.GROUP_WIDTH, ComponentAddress.GROUP_COUNT, address)
-    this.component = new MACAddress(address >> BigInt(ComponentAddress.GROUP_WIDTH * 2))
-    this.port = Number(address & 0xFFFFn)
+    this.OUI = address & ComponentAddress.MASK_OUI
+    this.NIC = address & ComponentAddress.MASK_NIC
+    this.TYP = address & ComponentAddress.MASK_TYP
+    this.NUM = address & ComponentAddress.MASK_NUM
+
+    this.componentMAC = new MACAddress(address >> BigInt(ComponentAddress.SHIFT_TYP + ComponentAddress.SHIFT_NUM))
+    this.id = this.TYP | this.NUM
   }
 
   public slash (mask: number): ComponentAddress {
@@ -38,6 +76,6 @@ export class ComponentAddress extends AbstractAddress {
    * @param port The port ID.
    */
   public forPort (port: number): ComponentAddress {
-    return ComponentAddress.fromAddresses(this.component, port)
+    return ComponentAddress.fromAddresses(this.componentMAC, port)
   }
 }
--- src/core/comp/component.ts
+++ src/core/comp/component.ts
@@ -7,9 +7,6 @@ import { IPortOpts, Port } from './port'
  */
 export abstract class Component {
 
-  /** The mask used to extract the port number from a component address */
-  public static readonly COMP_PORT_MASK = 0xFF00
-
   /** Link input bit */
   public static readonly COMP_PORT_BIT_IN = 1 << 7
 
@@ -40,8 +37,8 @@ export abstract class Component {
   public getNextAvailablePortNumber (type: number): number {
     type &= 0xFF
 
-    for (let i = 0; i <= 0xFF; i++) {
-      if (!this.ports.has(i << 4 | type)) return i
+    for (let i = 0; i < 2 ** 8; i++) {
+      if (!this.ports.has(i | (type << ComponentAddress.SHIFT_TYP))) return i
     }
 
     throw new Error('No available port numbers for type')
@@ -53,7 +50,7 @@ export abstract class Component {
    * @see createNextPort
    */
   public createPort (opts: IPortOpts): Port {
-    const pid = (opts.id << 4 && Component.COMP_PORT_MASK) | Port.getType(opts)
+    const pid = (opts.id && Number(ComponentAddress.MASK_NUM)) | (Port.getType(opts) << ComponentAddress.SHIFT_TYP)
     const port = new Port(this, pid)
 
     if (this.ports.has(port.id)) throw new Error('Port address already in use')
